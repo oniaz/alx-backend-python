1bd62ddb13edc5917eceb7c5080c6fd63b402d7d
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-06 22:57:32 +0300

    task 3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Type-annotated to_str function."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation.
+
+    Args:
+        n (float): A float number.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
