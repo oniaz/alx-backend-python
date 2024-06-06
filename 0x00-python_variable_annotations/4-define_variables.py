e6358f9618981e575de777b1660d8676164d573f
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-06 23:03:48 +0300

    task 4. Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module with annotated variables."""
+
+
+a: int = 1
+"""int: An integer variable."""
+
+pi: float = 3.14
+"""float: A floating-point variable representing the value of pi."""
+
+i_understand_annotations: bool = True
+"""bool: A boolean variable indicating whether annotations are understood."""
+
+school: str = 'Holberton'
+"""str: A string variable representing the name of a school."""
