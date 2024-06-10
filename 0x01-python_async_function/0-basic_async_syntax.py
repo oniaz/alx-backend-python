4e221737cd78cef48d1729a062c12f64e9cdf498
oniaz <32391049+oniaz@users.noreply.github.com>
2024-06-10 17:57:02 +0300

    Add type for the return value

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Asynchronously waits for a random amount of time.
 
     Args:
