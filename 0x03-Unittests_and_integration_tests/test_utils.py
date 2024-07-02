94ce72818f76f50c8bc1d71b27c970dfbbefefbb
oniaz <32391049+oniaz@users.noreply.github.com>
2024-07-02 04:39:17 +0300

    task 3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,10 +4,10 @@
 """
 
 import unittest
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Callable
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -66,6 +66,39 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test the memoize decorator function
+    """
+    def test_memoize(self) -> None:
+        """
+        Test that a_property is memoized
+        """
+        class TestClass:
+            """ Test Class """
+            def a_method(self):
+                """ a method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ a property """
+                return self.a_method()
+
+        with patch.object(
+                TestClass, 'a_method', return_value=42) as mock_method:
+
+            obj = TestClass()
+
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
